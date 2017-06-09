f75ea37a6e858afa5327da414e749945181b8645
August Seiple <aseiple678@gmail.com>
2017-06-09 00:44:54 -0400

    First content commit

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour {
+    private void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKeyDown("e"))
+        {
+            Debug.Log("Pressed");
+        }
+    }
+}
